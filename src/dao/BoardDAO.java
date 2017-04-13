17175f2144f55113304e08853bb8b113b39cbe21
kodica0311 <kodica0311@kodica0311-PC>
2017-04-13 12:22:45 +0900

    BoardDAO내용삭제

--- src/dao/BoardDAO.java
+++ src/dao/BoardDAO.java
@@ -1,7 +1,5 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
@@ -10,13 +8,6 @@ import conn.DBConn;
 
 public class BoardDAO {
 	
-	
-	
-	//게시판 등록
-	
-	//게시판 수정
-	
-	//게시판 삭제
 	
 	
 }
