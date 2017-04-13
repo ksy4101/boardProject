02abf10fd7966508ab23051f80b9881999069727
kodica0317 <kodica0317@kodica0317-PC>
2017-04-13 17:35:09 +0900

    등록 & 수정

--- src/dao/ReplyDAO.java
+++ src/dao/ReplyDAO.java
@@ -1,64 +1,68 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.awt.*;
+import java.awt.Event.*;
 
-import conn.DBConn;
 import vo.ReplyVO;
+import conn.DBConn;
 
 public class ReplyDAO {
 
-	// 댓글 등록
 	public void insertReply(ReplyVO reply) throws SQLException {
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 
-		int reNO = 0;
 		try {
 			conn = DBConn.getConnection();
 
 			StringBuilder sql = new StringBuilder();
-
-			sql.append("insert into reply(re_no,art_no,mem_id,re_content,re_date)");
-			// 시퀀스 게시글 번호,등록하는 아이디,댓글 내용,등록날짜
-			sql.append("values(RE_NO_SEQ.nextval, ? ,? , ?, sysdate)");
+			sql.append("insert into reply (re_no, art_no, mem_id, re_content, re_date)  ");
+			sql.append("values (reNo_seq.nextval, ?, ?, ?, sysdate) "); 
 			pstmt = conn.prepareStatement(sql.toString());
 
-			pstmt.setInt(1, reply.getArtNo()); //
+			pstmt.setInt(1, reply.getArtNo());
 			pstmt.setString(2, reply.getMemId());
 			pstmt.setString(3, reply.getReContent());
 
 			pstmt.executeUpdate();
 			pstmt.close();
 
-		} finally {
-			if (pstmt != null) pstmt.close();
-			if (conn != null) conn.close();			
+		}  finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
 		}
-
 	}
-	
-	
-
-	// 댓글 수정
 	public void updateReply(ReplyVO reply) throws SQLException {
-
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		try {
+		
+		try{
 			conn = DBConn.getConnection();
-
+			
 			StringBuilder sql = new StringBuilder();
-
-		} finally {
-			if (pstmt != null) pstmt.close();
-			if (conn != null) conn.close();
+			sql.append("update  reply  ");
+			sql.append("set re_content = ?	");
+			sql.append("where re_id = ? "); // 댓글번호가 같은 경우 수정
+			System.out.println(sql.toString());
+			pstmt = conn.prepareStatement(sql.toString());
+			
+			pstmt.setString(1, reply.getReContent());
+			pstmt.setInt(2, reply.getReNo());
+			
+		}	finally {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
 			
 		}
-		
 	}
-
 }
