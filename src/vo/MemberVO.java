34fd8154e31cbf97deca692de1deae879b330ae8
kodica0305 <kodica0305@kodica0305-PC>
2017-04-13 11:04:30 +0900

    회원vo수정

--- src/vo/MemberVO.java
+++ src/vo/MemberVO.java
@@ -2,4 +2,151 @@ package vo;
 //학생 클래스 같은거에여
 public class MemberVO {
 
+	private String memId;
+	private String jumin;
+	private String password;
+	private String name;
+	private String postNo;
+	private String phone;
+	private String email;
+	private String memo;
+	private int grade;
+	private String address;
+	
+	
+	public MemberVO() {
+		super();
+	}
+
+
+	public MemberVO(String memId, String jumin, String password, String name, String postNo, String phone, String email,
+			String memo, int grade, String address) {
+		super();
+		this.memId = memId;
+		this.jumin = jumin;
+		this.password = password;
+		this.name = name;
+		this.postNo = postNo;
+		this.phone = phone;
+		this.email = email;
+		this.memo = memo;
+		this.grade = grade;
+		this.address = address;
+	}
+	
+	
+
+
+	public String getMemId() {
+		return memId;
+	}
+
+
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+
+
+	public String getJumin() {
+		return jumin;
+	}
+
+
+	public void setJumin(String jumin) {
+		this.jumin = jumin;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPostNo() {
+		return postNo;
+	}
+
+
+	public void setPostNo(String postNo) {
+		this.postNo = postNo;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getMemo() {
+		return memo;
+	}
+
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+
+	public int getGrade() {
+		return grade;
+	}
+
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	@Override
+	public String toString() {
+		return "MemberVO [memId=" + memId + ", jumin=" + jumin + ", password=" + password + ", name=" + name
+				+ ", postNo=" + postNo + ", phone=" + phone + ", email=" + email + ", memo=" + memo + ", grade=" + grade
+				+ ", address=" + address + "]";
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/vo/PostVO.java
+++ src/vo/PostVO.java
@@ -1,5 +1,98 @@
 package vo;
 
 public class PostVO {
+	private int postNo;
+	private String sido;
+	private String gugun;
+	private String dong;
+	private String bunzi;
+	private String zipcode;
+	
+	
+	public PostVO() {
+		super();
+	}
 
+
+	public PostVO(int postNo, String sido, String gugun, String dong, String bunzi, String zipcode) {
+		super();
+		this.postNo = postNo;
+		this.sido = sido;
+		this.gugun = gugun;
+		this.dong = dong;
+		this.bunzi = bunzi;
+		this.zipcode = zipcode;
+	}
+
+
+	public int getPostNo() {
+		return postNo;
+	}
+
+
+	public void setPostNo(int postNo) {
+		this.postNo = postNo;
+	}
+
+
+	public String getSido() {
+		return sido;
+	}
+
+
+	public void setSido(String sido) {
+		this.sido = sido;
+	}
+
+
+	public String getGugun() {
+		return gugun;
+	}
+
+
+	public void setGugun(String gugun) {
+		this.gugun = gugun;
+	}
+
+
+	public String getDong() {
+		return dong;
+	}
+
+
+	public void setDong(String dong) {
+		this.dong = dong;
+	}
+
+
+	public String getBunzi() {
+		return bunzi;
+	}
+
+
+	public void setBunzi(String bunzi) {
+		this.bunzi = bunzi;
+	}
+
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+
+	@Override
+	public String toString() {
+		return "PostVO [postNo=" + postNo + ", sido=" + sido + ", gugun=" + gugun + ", dong=" + dong + ", bunzi="
+				+ bunzi + ", zipcode=" + zipcode + "]";
+	}
+	
+	
+	
+	
+	
 }
