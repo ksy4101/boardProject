67eb80df86ecd20b740faff3539b4b6668bbf038
kodica0317 <kodica0317@kodica0317-PC>
2017-04-13 12:49:06 +0900

    추가

--- src/dao/ReplyDAO.java
+++ src/dao/ReplyDAO.java
@@ -1,5 +1,46 @@
 package dao;
 
-public class ReplayDAO {
+import java.sql.*;
+import java.awt.*;
+import java.awt.Event.*;
 
+import vo.ReplyVO;
+import conn.DBConn;
+
+public class ReplyDAO {
+
+	public void insertReply(ReplyVO reply) throws SQLException {
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = DBConn.getConnection();
+
+			StringBuilder sql = new StringBuilder();
+			sql.append("insert into reply (reNo, memId, reContent, reDate) ");
+			sql.append("values (?, ?, ?, sysdate)");
+			pstmt = conn.prepareStatement(sql.toString());
+
+			pstmt.setInt(1, reply.getReNo());
+			pstmt.setString(2, reply.getMemId());
+			pstmt.setString(3, reply.getReContent());
+
+			pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
 }
