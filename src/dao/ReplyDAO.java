24823be5f9fe96b7bf1d8229295a9644e1959fd5
kodica0316 <kodica0316@kodica0316-PC>
2017-04-19 17:56:02 +0900

    댓글 최종 업데이트

--- src/dao/ReplyDAO.java
+++ src/dao/ReplyDAO.java
@@ -20,20 +20,21 @@ public class ReplyDAO {
 		
 		
 		try {
-			conn = DBConn.getConnection();
+			conn = DBConn.getConnection(); //생성자에 Connection 정보를 넘김
 
-			StringBuilder sql = new StringBuilder();
+			StringBuilder sql = new StringBuilder(); //기존에 있는 객체의 크기를 증가시키면서 값을 더한다.
+			
 
 			sql.append("insert into reply(re_no,art_no,mem_id,re_content,re_date)");
 			// 시퀀스 게시글 번호,등록하는 아이디,댓글 내용,등록날짜
 			sql.append("values(RE_NO_SEQ.nextval, ? ,? , ?, sysdate)");
-			pstmt = conn.prepareStatement(sql.toString());
+			pstmt = conn.prepareStatement(sql.toString()); //미리 SQL 쿼리를 분석해 놓는다
 
-			pstmt.setInt(1, reply.getArtNo()); //
-			pstmt.setString(2, reply.getMemId());
-			pstmt.setString(3, reply.getReContent());
+			pstmt.setInt(1, reply.getArtNo()); //게시글 번호를 넣는다
+			pstmt.setString(2, reply.getMemId()); //로그인된 아이디를 넣는다.
+			pstmt.setString(3, reply.getReContent());//댓글내용를 넣는다
 
-			pstmt.executeUpdate();
+			pstmt.executeUpdate();//Statement 객체를 통해 쿼리를 수행한다.
 			pstmt.close();
 
 		} catch (Exception e) {
@@ -62,7 +63,7 @@ public class ReplyDAO {
 			StringBuilder sql = new StringBuilder();
 			sql.append("update  reply                    												");
 			sql.append("set re_content = ?																");
-			sql.append("where re_no = ?																	");
+			sql.append("where re_no = ?															");
 			pstmt = conn.prepareStatement(sql.toString());
 			
 			pstmt.setString(1, reply.getReContent());
@@ -70,6 +71,8 @@ public class ReplyDAO {
 			
 			
 			pstmt.executeUpdate();
+		
+			conn.commit();
 			
 		}
 		catch (Exception e) {
@@ -117,39 +120,11 @@ public class ReplyDAO {
 		}
 	}
 	
-	//댓글 삭제
-		public void deletetReply(String memId) throws SQLException {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			try {
-				conn = DBConn.getConnection();
-				
-				StringBuilder sql = new StringBuilder();
-				sql.append("delete from reply					");
-				sql.append("where mem_id = ?					");
-				pstmt = conn.prepareStatement(sql.toString());
-				
-				pstmt.setString(1, memId);
-				
-				pstmt.executeUpdate();
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			finally {
-				try {
-					if (pstmt != null) pstmt.close();
-					if (conn != null) conn.close();
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
-			}
-		}
-		
+	
 	
 	//내 댓글 조회
-	public List<ReplyVO> selectMyReply(String memId) throws SQLException {
-		ArrayList<ReplyVO> replys = new ArrayList<ReplyVO>();
+	public List<ReplyVO> selectMyReply( int artNo,String memId) throws SQLException {
+		List<ReplyVO> replys = new ArrayList<ReplyVO>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -162,23 +137,27 @@ public class ReplyDAO {
 			StringBuilder sql = new StringBuilder();
 			
 			sql.append("select re_no,art_no,re_content, mem_id,re_date		");
-			sql.append("from reply											");
-			sql.append("where memId = ?										");
-			sql.append("order by re_date DESC								");
+			sql.append("from reply								");
+			sql.append("where art_no = ? and mem_id = ?						");
+			sql.append("order by re_date DESC					");
 			
 			pstmt = conn.prepareStatement(sql.toString());
-			pstmt.setString(1, memId);
-			rs = pstmt.executeQuery(sql.toString());
+			pstmt.setInt(1, artNo);
+			pstmt.setString(2, memId);
+			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
 				ReplyVO reply = new ReplyVO();
 				reply.setReNo(rs.getInt(1));
-				reply.setReNo(rs.getInt(2));
+				reply.setArtNo(rs.getInt(2));
 				reply.setReContent(rs.getString(3));
-				memId = rs.getString(4);
+				reply.setMemId(rs.getString(4));
 				reply.setReDate(rs.getString(5));
 				replys.add(reply);
+			
+				
 			}
+			return replys;
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -193,12 +172,12 @@ public class ReplyDAO {
 		}
 		
 
-		return replys;
+		return null;
 	}
 	
 	//전체 댓글 조회
 		public List<ReplyVO> selectReplyList(int artNo) throws SQLException {
-			ArrayList<ReplyVO> replys = new ArrayList<ReplyVO>();
+			List<ReplyVO> replys = new ArrayList<ReplyVO>();
 			Connection conn = null;
 			PreparedStatement pstmt = null;
 			ResultSet rs = null;
@@ -212,21 +191,28 @@ public class ReplyDAO {
 				
 				sql.append("select re_no,art_no,re_content, mem_id,re_date		");
 				sql.append("from reply								");
-				sql.append("where artNo = ?							");
+				sql.append("where art_no = ?							");
 				sql.append("order by re_date desc					");
+				
 				pstmt = conn.prepareStatement(sql.toString());
 				pstmt.setInt(1, artNo);
-				rs = pstmt.executeQuery(sql.toString());
+				rs = pstmt.executeQuery();
 				
 				while(rs.next()) {
 					ReplyVO reply = new ReplyVO();
+					
 					reply.setReNo(rs.getInt(1));
-					artNo = rs.getInt(2);
+					reply.setArtNo(rs.getInt(2));
 					reply.setReContent(rs.getString(3));
 					reply.setMemId(rs.getString(4));
 					reply.setReDate(rs.getString(5));
+					
 					replys.add(reply);
 				}
+				
+				
+				
+				return replys;
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -239,8 +225,9 @@ public class ReplyDAO {
 					e2.printStackTrace();
 				}
 			}
+			return null;
 
-			return replys;
+			
 		}
 
 }
